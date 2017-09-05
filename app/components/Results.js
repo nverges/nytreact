35a4a51916ae7a23413babe3b29d9441852414a1
nverges <nverges@email.arizona.edu>
2017-09-04 20:09:01 -0700

    layout

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,18 +1,27 @@
 var React = require("react");
 
 var Results = React.createClass({
+
+  getInitialState: function() {
+    return {
+      title: "Search Results",
+      articleTitle: "Article Title Here",
+      articleDetails: "Article Details Here"
+    }
+  },
+
   render: function () {
     return (
       <div className="panel panel-primary">
-        <div className="panel-heading">Results Panel</div>
+        <div className="panel-heading">{this.state.title}</div>
 
         <div className="panel-body">
 
             <div className="panel panel-default">
-                <div className="panel-heading">Result <button type="button" className="btn btn-primary pull-right btn-sm RbtnMargin">Save</button></div>
+                <div className="panel-heading">{this.state.articleTitle} <button type="button" className="btn btn-primary pull-right btn-sm RbtnMargin">Save</button></div>
 
                 <div className="panel-body">
-                    Result Details
+                    {this.state.articleDetails}
                 </div>
             </div>
 
--- app/components/Saved.js
+++ app/components/Saved.js
@@ -1,18 +1,27 @@
 var React = require("react");
 
 var Saved = React.createClass({
+
+  getInitialState: function() {
+      return {
+        title: "Saved Articles",
+        savedArticleTitle: "Saved Article Title Here",
+        savedArticleDetails: "Saved Article Details Here"
+      }
+  },
+  
   render: function () {
     return (
       <div className="panel panel-primary">
-        <div className="panel-heading">Saved Results</div>
+        <div className="panel-heading">{this.state.title}</div>
 
         <div className="panel-body">
 
             <div className="panel panel-default">
-                <div className="panel-heading">Saved Result <button type="button" className="btn btn-primary pull-right btn-sm RbtnMargin">Primary</button></div>
+                <div className="panel-heading">{this.state.savedArticleTitle} <button type="button" className="btn btn-primary pull-right btn-sm RbtnMargin">Delete</button></div>
 
                 <div className="panel-body">
-                    Saved Result Details
+                    {this.state.savedArticleDetails}
                 </div>
             </div>
 
